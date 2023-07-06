7d574ade168793f7c0fd2c01a5e164edb989bb49
chimcha67 <chimchatitus@gmail.com>
2023-07-06 04:00:35 +0100

    updateon headers

--- Routs/index.routes.js
+++ Routs/index.routes.js
@@ -2,7 +2,9 @@ module.exports=function(app){
     app.use(function(res, req, next){
         res.header(
             "Access-Controll-Allow-Headers",
-            "x-access-token, Origin, Content-Type, Accept"
+            "x-access-token, Origin, Content-Type, Accept",
+            'Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE',
+            'Access-Control-Allow-Credentials', true
         );
         next()
     })
